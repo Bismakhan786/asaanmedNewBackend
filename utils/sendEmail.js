cd72830073b76030c0380e58752112a2af616b37
Bismakhan786 <bismaashfaq009@gmail.com>
2023-03-03 19:20:19 +0500

    final 2

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -29,6 +29,7 @@ const sendEmail = async ({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     service: process.env.SMTP_SERVICE,
+    secure: true,
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
